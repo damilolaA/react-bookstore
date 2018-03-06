02c87fe9eced725639b4b3d32cc83245ef904118
dammy <adiodamilola@yahoo.com>
2018-03-06 17:20:21 +0100

    fix adminData validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,37 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Form = () => {
+class Form extends Component{
 
-	return(
-		<div className="wrapper">
-			<h1 id="register-label">Register</h1>
-			<hr/>
-			<form id="register">
-				<div>
-					<label>first name:</label>
-					<input type="text" name="fname" placeholder="first name"/>
-				</div>
-				<div>
-					<label>last name:</label>	
-					<input type="text" name="lname" placeholder="last name"/>
-				</div>
+	constructor() {
+		super()
+		this.state = {
+			errorMessages:{},
+			"fname": "",
+			"lname": "",
+			"email": "",
+			"password": ""
+		}
 
-				<div>
-					<label>email:</label>
-					<input type="text" name="email" placeholder="email"/>
-				</div>
-				<div>
-					<label>password:</label>
-					<input type="password" name="password" placeholder="password"/>
-				</div>
+		this.submitForm = this.submitForm.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-				<input type="submit" name="register" value="register"/>
-			</form>
+	submitForm(e) {
+		e.preventDefault();
 
-			<h4 className="jumpto">Have an account? <a href="">login</a></h4>
-		</div>
-	);
-	
+		this.validateForm();
+	}
+
+	handleChange(e) {
+		let dummy = {},
+		input = e.target.name;
+
+		dummy[input] = e.target.value;
+
+		this.setState(dummy);
+	}
+
+	validateForm() {
+		let data = this.state,
+			errors = {errorMessages:{}};
+
+		for(let value in data) {
+			if(data[value] === "") {
+				errors.errorMessages[value] = "Please enter your " + value;
+				this.setState(errors);
+				return;
+			}
+		}
+	}
+
+	render() {
+		console.log(this.state);
+		return(
+			<div className="wrapper">
+				<h1 id="register-label">Register</h1>
+				<hr/>
+				<form id="register" onSubmit={this.submitForm}>
+					<div>
+						<p className='err'>{this.state.errorMessages.fname ? this.state.errorMessages.fname : ""}</p>
+						<label>first name:</label>
+						<input onChange={this.handleChange} type="text" name="fname" placeholder="first name"/>
+					</div>
+
+					<div>
+						<p className='err'>{this.state.errorMessages.lname ? this.state.errorMessages.lname : ""}</p>
+						<label>last name:</label>	
+						<input onChange={this.handleChange} type="text" name="lname" placeholder="last name"/>
+					</div>
+
+					<div>
+						<p className='err'>{this.state.errorMessages.email ? this.state.errorMessages.email : ""}</p>
+						<label>email:</label>
+						<input onChange={this.handleChange} type="text" name="email" placeholder="email"/>
+					</div>
+
+					<div>
+						<p className='err'>{this.state.errorMessages.password ? this.state.errorMessages.password : ""}</p>
+						<label>password:</label>
+						<input onChange={this.handleChange} type="password" name="password" placeholder="password"/>
+					</div>
+
+					<input type="submit" name="register" value="register"/>
+				</form>
+
+				<h4 className="jumpto">Have an account? <a href="">login</a></h4>
+			</div>
+		);
+	}
 }
 
 export default Form;
