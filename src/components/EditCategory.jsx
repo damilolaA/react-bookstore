2e34d5ec9111d9eecf78642141caf8fc97dafcb5
dammy <adiodamilola@yahoo.com>
2018-03-08 03:38:35 +0100

    use axios to make a put request to category endpoint

--- src/components/EditCategory.jsx
+++ src/components/EditCategory.jsx
@@ -1,32 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './Header';
 import Footer from './Footer';
 
-const EditCategory = (props) => {
+class EditCategory extends Component{
 
-	let categoryName = props.match.params.id;
+	constructor(props) {
+		super(props)
+		this.state = {
+			categoryName: {}
+		}
 
-	categoryName = categoryName.substring(2);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-	return (
-		<div>
-			<Header nav />
-			<div className="wrapper">
-	          <h1 id="register-label">Edit Category</h1>
-	          <hr />
-	          <form id="register">
+	handleSubmit(e) {
+		e.preventDefault();
 
-	            <div>
-	              <label>category name:</label>
-	              <input type="text" name="categoryName" value={categoryName} />
-	            </div>
+		this.postEditedCategory();
 
-	            <input type="submit" name="category" value="Edit" />
-	          </form>
+	}
+
+	postEditedCategory() {
+		let editedName = this.state,
+	    id = this.props.match.params.id;
+
+		id = id.charAt(0);
+		
+		if(editedName.hasOwnProperty('categoryName')) {
+			axios({
+				method: "put",
+				url: `https://bookstoreappapi.herokuapp.com/api/v1/category/${id}`,
+				data: editedName
+			})
+			.then(response => {
+				console.log(response);
+			})
+			.catch(err => {
+				console.log(err);
+			})
+		} else {
+			console.log('could not edit category');
+		}
+	}
+
+	handleChange(e) {
+
+		let data = this.state,
+			inputName = e.target.name;
+
+		 console.log(data);
+		data[inputName] = e.target.value;
+		console.log(data);
+		this.setState(data);
+	}
+
+	render() {
+		/*let categoryName = this.props.match.params.id;
+
+		categoryName = categoryName.substring(2);*/
+
+		console.log(this.state);
+		return (
+			<div>
+				<Header nav />
+				<div className="wrapper">
+		          <h1 id="register-label">Edit Category</h1>
+		          <hr />
+		          <form id="register" onSubmit={this.handleSubmit}>
+
+		            <div>
+		              <label>category name:</label>
+		              <input onChange={this.handleChange} type="text" name="categoryName"  />
+		            </div>
+
+		            <input type="submit" name="category" value="Edit" />
+		          </form>
+				</div>
+				<Footer />
 			</div>
-			<Footer />
-		</div>
-	);
+		);
+	}
 }
 
 export default EditCategory;
