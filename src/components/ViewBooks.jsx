36852e6e5a4701cab246697276b0961836370c6b
dammy <adiodamilola@yahoo.com>
2018-03-10 12:52:20 +0100

    create DisplayBooks components to render books fetched from the server

--- src/components/ViewBooks.jsx
+++ src/components/ViewBooks.jsx
@@ -1,35 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './Header';
 import Footer from './Footer';
 
-const ViewBooks = () => {
+class ViewBooks extends Component {
 
-	return (
-		<div>
-			<Header nav/>
-			<div className="wrapper">
-	          <div id="stream">
-	            <table id="tab">
-	              <thead>
-	                <tr>
-	                  	<th>title</th>
-						<th>author</th>
-						<th>price</th>
-						<th>category</th>
-						<th>publication date</th>
-						<th>image</th>
-						<th>edit</th>
-						<th>delete</th>
-	                </tr>
-	              </thead>
+	constructor() {
+		super()
+		this.state = {
+			bookData: {}
+		}
+	}
 
-	              
-	            </table>
-	          </div>
-	        </div>
-			<Footer />
-		</div>
-	)
+	componentDidMount() {
+	    axios
+	      .get('https://bookstoreappapi.herokuapp.com/api/v1/books')
+	      .then(response => {
+	        this.setState({ bookData: { data: response.data } });
+	      })
+	      .catch(err => {
+	        console.log(err);
+	      });
+	}
+
+	removeBook(name, i) {
+	    let book = this.state.bookData.data;
+
+	    book.splice(i, 1);
+
+	    this.setState({
+	      book
+	    });
+	}
+
+	render() {
+		return (
+			<div>
+				<Header nav/>
+				<div className="wrapper">
+		          <div id="stream">
+		            <table id="tab">
+		              <thead>
+		                <tr>
+		                  	<th>title</th>
+							<th>author</th>
+							<th>price</th>
+							<th>category</th>
+							<th>publication date</th>
+							<th>image</th>
+							<th>edit</th>
+							<th>delete</th>
+		                </tr>
+		              </thead>
+		              
+		            </table>
+		          </div>
+		        </div>
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default ViewBooks
