86947bb072d956396f1325130c11727dc9daef68
damilolo <adiodamilola@yahoo.com>
2018-03-17 03:25:01 -0700

    use bootstrap to style admin landing page

--- src/components/AdminLanding.jsx
+++ src/components/AdminLanding.jsx
@@ -4,7 +4,24 @@ const Landing = () => {
 
 	return (
 		<div>
-			<h3>Welcome Admin</h3>
+			<nav className="navbar navbar-light bg-light static-top">
+		      <div className="container">
+		        <a className="btn btn-primary" href="/login">Login</a>
+		        <a className="btn btn-primary" href="/register">Sign Up</a>
+		      </div>
+		    </nav>
+
+		    <header className="masthead text-white text-center">
+		      <div className="overlay"></div>
+		      <div className="container">
+		        <div className="row">
+		          <div className="col-xl-9 mx-auto">
+		            <h1 className="mb-5">React Bookstore</h1>
+		            <h3 className="mb-5">Login or Register if you are registered</h3>
+		          </div>
+		        </div>
+		      </div>
+    		</header>
 		</div>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
+import './landing-page.css';
+import './vendor/bootstrap/css/bootstrap.min.css';
+import './vendor/font-awesome/css/font-awesome.min.css';
+import './vendor/simple-line-icons/css/simple-line-icons.css';
+import './landing-page.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
